dfcd984ad99a896a4e20f06b0d6d783ebfbd0065
Shashhidhar B <shashishashidhar70@gmail.com>
2018-07-16 14:50:53 +0530

    aggregate gdp and pop of g20 countries assignment done - using asynchronous functions

--- aggregate.js
+++ aggregate.js
@@ -4,20 +4,12 @@
  */
 const fs = require('fs');
 
-<<<<<<< HEAD
 const aggregate = filePath => new Promise((resolveag, rejectag) => {
   const countryContinentArray = file => new Promise((resolve, reject) => {
     fs.readFile(file, 'utf8', (err, items) => {
       if (err) {
         reject(err);
       } else {
-=======
-const aggregate = (filePath) => {
-  const countryContinentMap = file => new Promise((resolve, reject) => {
-    fs.readFile(file, 'utf8', (err, items) => {
-      if (err) reject(err);
-      else {
->>>>>>> 7095ecf0fdd0cb29e485054495573d8feb12af11
         const splitRow = items.split('\n');
         let splitIndi;
         const AllCCMap = new Map();
@@ -28,7 +20,6 @@ const aggregate = (filePath) => {
         resolve(AllCCMap);
       }
     });
-<<<<<<< HEAD
   });
   const CCGdpPop = filePath2 => new Promise((resolve1, reject1) => {
     fs.readFile(filePath2, 'utf8', (err, contents) => {
@@ -86,64 +77,5 @@ const aggregate = (filePath) => {
     });
   });
 });
-=======
-
-    countryContinentMap('./cc-mapping.txt').then((AllCCMap) => {
-      const CCGdpPop = () => new Promise((resolve1, reject1) => {
-        fs.readFile(filePath, 'utf8', (err, contents) => {
-          if (err) reject1(err);
-          else {
-            const CCpopgdpArray = [];
-            const rowSplit = contents.split('\n');
-            let indiSplit;
-            for (let i = 1; i < rowSplit.length - 1; i += 1) {
-              indiSplit = rowSplit[i].split(',');
-              if (indiSplit[0].slice(1, -1) !== 'European Union') {
-                CCpopgdpArray[i] = [AllCCMap.get(indiSplit[0].slice(1, -1)),
-                  indiSplit[0].slice(1, -1), indiSplit[4].slice(1, -1), indiSplit[7].slice(1, -1)];
-              }
-            }
-            resolve1(CCpopgdpArray);
-          }
-        });
-      });
-      CCGdpPop().then((CCpopgdpArray) => {
-        const continentgdpMap = new Map();
-        const continentpopMap = new Map();
-        CCpopgdpArray.forEach((arrayElement) => {
-          if (continentpopMap.has(arrayElement[0])) {
-            continentpopMap.set(arrayElement[0],
-              parseFloat(continentpopMap.get(arrayElement[0])) + parseFloat(arrayElement[2]));
-          } else {
-            continentpopMap.set(arrayElement[0], parseFloat(arrayElement[2]));
-          }
-
-          if (continentgdpMap.has(arrayElement[0])) {
-            continentgdpMap.set(arrayElement[0],
-              parseFloat(continentgdpMap.get(arrayElement[0])) + parseFloat(arrayElement[3]));
-          } else {
-            continentgdpMap.set(arrayElement[0], parseFloat(arrayElement[3]));
-          }
-        });
-        const outputObject = {};
-        continentgdpMap.forEach((arrayElement, key) => {
-          outputObject[key] = {
-            GDP_2012: arrayElement,
-            POPULATION_2012: continentpopMap.get(key),
-          };
-        });
-        const writingAsyncly = () => new Promise((resolve2, reject2) => {
-          const outputpath = './output/output.json';
-          fs.writeFile(outputpath, JSON.stringify(outputObject, 2, 2), (err) => {
-            if (err) reject2(err);
-            else resolve2();
-          });
-        });
-        writingAsyncly();
-      });
-    });
-  });
-};
->>>>>>> 7095ecf0fdd0cb29e485054495573d8feb12af11
 
 module.exports = aggregate;
